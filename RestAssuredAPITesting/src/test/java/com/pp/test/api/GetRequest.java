0be3fc505157dc6d52710fcd7a413f9fba5d7031
prashant <prashant.prusty@gmail.com>
2019-08-07 16:19:38 +0530

    1st commit to master branch

---
+++ RestAssuredAPITesting/src/test/java/com/pp/test/api/GetRequest.java
@@ -0,0 +1,45 @@
+package com.pp.test.api;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
+public class GetRequest {
+	
+@Test	
+void getRequest(){
+	
+	//Specify baseURI
+	RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city/";
+	
+	//Request Object
+	RequestSpecification httprequest = RestAssured.given();
+	
+	
+	// Response Object
+	Response respns = httprequest.request(Method.GET,"Hyderabad");
+	
+	// Print response in console window
+	
+	String strRsponseBody =respns.getBody().asString();
+	System.out.println("ResponseBody Is" +strRsponseBody);
+	
+	//Satus code validation
+	int statusCode = respns.getStatusCode();
+	System.out.println("Statsu Code is : "+ statusCode);
+	Assert.assertEquals(statusCode, 201);
+	
+	// Status line validation
+	
+	String statusLine= respns.statusLine();
+	System.out.println("Status Line is : "+statusLine);
+	Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+	
+	
+}
+
+}
---
+++ RestAssuredAPITesting/src/test/java/com/pp/test/api/PostRequest.java
@@ -0,0 +1,54 @@
+package com.pp.test.api;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
+
+public class PostRequest {
+	
+	@Test
+	void postRequest(){
+	
+		//Specify base URI
+		RestAssured.baseURI ="http://restapi.demoqa.com/customer";
+		
+			
+		// Request object
+		RequestSpecification httpRequest=RestAssured.given();
+		
+		//Request payload sending along with post request
+		
+		JSONObject requestParams=new JSONObject();
+		
+		requestParams.put("FirstName","ppfname");
+		requestParams.put("LastName","pplname");
+		requestParams.put("UserName","Uname");
+		requestParams.put("Password","password123");
+		
+		httpRequest.header("Content-Type","application/json");
+		httpRequest.body(requestParams.toJSONString());
+		
+		// Response Object
+		Response response = httpRequest.request(Method.POST,"/register"); 
+		
+		String responseBody=response.getBody().asString();
+		System.out.println("getBody Response is"+responseBody);
+		
+		
+		// Status code validation
+		
+		int statusCode=response.getStatusCode();
+		System.out.println("statusCode is " +statusCode);
+		
+		Assert.assertEquals(statusCode, 201);
+		
+		
+		
+	}
+
+}
